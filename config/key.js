f2f24db0360268a572dbbd7fa82fe83cc375fd69
an-nguyen239 <anguyen108@student.gsu.edu>
2019-03-01 16:21:34 -0500

    set up mongodb and route

---
+++ config/key.js
@@ -0,0 +1,4 @@
+module.exports = {
+	mongoURI: 
+	'mongodb://annguyen:annguyen1@ds157735.mlab.com:57735/shoppingsystem'
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+
+
+const app = express();
+
+//DB config
+const db = require('./config/key').mongoURI;
+
+//connect to mongodb
+mongoose
+	.connect(db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello'));
+
+//Use routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
