c3d0a6e2c2ed612dde4d1d92566a3fd7a81569e9
an-nguyen239 <anguyen108@student.gsu.edu>
2019-03-01 16:38:30 -0500

    Create register route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,46 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
+//Load User model
+const User = require('../../models/User');
 
 
 // @route GET api/users/test
 // @desc Tests user route
 // @access Public
 router.get('/test', (req, res) => res.json({msg: "User works"}));
+
+// @route GET api/users/register
+// @desc Register user
+// access Public
+router.post('/register', (req, res) => {
+
+
+	User.findOne({ email: req.body.email })
+	.then(user => {
+		if (user) {
+			errors.email = 'Email already exists';
+			return res.status(400).json(errors);
+		} else {
+			
+			const newUser = new User({
+				name: req.body.name,
+				email: req.body.email,
+				password: req.body.password
+			});
+
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if(err) throw err;
+					newUser.password = hash;
+					newUser.save()
+						.then(user => res.json(user))
+						.catch(err => console.log(err))
+				})
+			})
+		}
+	})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport'); 
 
 const users = require('./routes/api/users');
 const posts = require('./routes/api/posts');
 
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./config/key').mongoURI;
