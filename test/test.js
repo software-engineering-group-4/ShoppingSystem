33f07c6ea7b1808d45345318e7166f5081901891
thompsonate <nate@natethompson.io>
2019-03-27 17:48:30 -0400

    add mocha and sample failing test

---
+++ test/test.js
@@ -0,0 +1,12 @@
+// import that doesn't work
+import { addItem } from '../client/src/actions/itemActions';
+
+// sample test
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
